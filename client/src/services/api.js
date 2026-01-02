54322aa0154fe9a441d3fe70ee156d487a31af37
madhumitha0511 <154619550+madhumitha0511@users.noreply.github.com>
2026-01-02 17:28:46 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@
 
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE = process.env.REACT_APP_API_URL ;
 
 // Create axios instance with interceptors
 const apiClient = axios.create({
